cc06c782cea3350308dc6cc4f1b3e76d5f8d598a
Arunsiva003 <a.arunsivaofficial@gmail.com>
2024-02-07 15:48:10 +0530

    put and code structured

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -97,9 +97,17 @@ fn handle_client(mut stream: TcpStream) {
     println!("Full request: {}", request);
 
     let (status_line, content) = match &*request {
-        r if r.starts_with("OPTIONS ") => (OK_RESPONSE.to_string(), "".to_string()),
+        r if r.starts_with("OPTIONS ") => (OK_RESPONSE.to_string(), "".to_string()), //options - to deal cors policy
+        r if r.starts_with("GET /api/rust/users") => {   // - to get all user details or specific user detail
+            if let Some(id) = parse_user_id(r) {
+                let id_str = id.to_string();
+                handle_get_user_request(&id_str)
+            } else {
+                handle_get_all_request(r)
+            }
+        },
+        r if r.starts_with("PUT /api/rust/users/") => handle_update_request(r),
         r if r.starts_with("POST /api/rust/users/login") => handle_login_request(r),
-        r if r.starts_with("GET /api/rust/users") => handle_get_all_request(r),
         r if r.starts_with("POST /api/rust/users") => handle_post_request(r),
         _ => (NOT_FOUND.to_string(), "404 not found".to_string()),
     };
@@ -140,52 +148,138 @@ fn handle_post_request(request: &str) -> (String, String) {
             };
 
             // Check if email and username are not already present
-            if is_email_username_available(&user.email, &user.username, &mut client) {
-                // Insert the user into the database
-                match client.query_one(
-                    "INSERT INTO users (firstname, lastname, username, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id",
-                    &[&user.firstname, &user.lastname, &user.username, &user.email, &user.password]
-                ) {
-                    Ok(row) => {
-                        println!("got row");
-                        let user_id: i32 = row.get(0);
 
-                        match client.query_one(
-                            "SELECT id, firstname, lastname, username, email, password FROM users WHERE id = $1",
-                            &[&user_id],
-                        ) {
-                            Ok(row) => {
-                                println!("got row user");
-                                let user = User {
-                                    id: row.get(0),
-                                    firstname: row.get(1),
-                                    lastname: row.get(2),
-                                    username: row.get(3),
-                                    email: row.get(4),
-                                    password: row.get(5),
-                                };
-
-                                return (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap());
-                            }
-                            Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to retrieve created user".to_string()),
-                        }
-                    }
-                    Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to insert user".to_string()),
-                }
-            } else {
-                // User with the given email or username already exists
-                return (NOT_FOUND.to_string(),"User with the given email or username already exists".to_string());
+            if is_email_exists(&user.email, &mut client){
+                return (NOT_FOUND.to_string(),"User with the given email already exists".to_string());
             }
+            if is_username_exists(&user.username, &mut client){
+                return (NOT_FOUND.to_string(),"User with the given username already exists".to_string());
+            }
+
+            // Insert the user into the database
+            match client.query_one(
+                "INSERT INTO users (firstname, lastname, username, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+                &[&user.firstname, &user.lastname, &user.username, &user.email, &user.password]
+            ) {
+                Ok(row) => {
+                    println!("got row");
+                    let user_id: i32 = row.get(0);
+                    let user_id = user_id.to_string(); 
+                    handle_get_user_request(&user_id)
+                    // match client.query_one(
+                    //     "SELECT id, firstname, lastname, username, email, password FROM users WHERE id = $1",
+                    //     &[&user_id],
+                    // ) {
+                    //     Ok(row) => {
+                    //         println!("got row user");
+                    //         let user = User {
+                    //             id: row.get(0),
+                    //             firstname: row.get(1),
+                    //             lastname: row.get(2),
+                    //             username: row.get(3),
+                    //             email: row.get(4),
+                    //             password: row.get(5),
+                    //         };
+
+                    //         return (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap());
+                    //     }
+                    //     Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to retrieve created user".to_string()),
+                    // }
+                }
+                Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to insert user".to_string()),
+            }
+         
+    }
+    _ => return (NOT_FOUND.to_string(), "Internal error".to_string()),
+}
+
+}
+
+
+
+
+fn handle_get_user_request(user_id: &str) -> (String, String) {
+    
+    // Connect to the database
+    let mut client = match Client::connect(DB_URL, NoTls) {
+        Ok(client) => client,
+        Err(_) => return (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+    };
+
+    // Query the database to get user details based on ID
+    println!("user is ####################{}",user_id);
+    let user_id: i32 = user_id.trim().parse().unwrap();
+    match client.query_opt(
+        "SELECT id, firstname, lastname, username, email, password FROM users WHERE id = $1",
+        &[&user_id],
+    ) {
+        Ok(Some(row)) => {
+            let user = User {
+                id: row.get(0),
+                firstname: row.get(1),
+                lastname: row.get(2),
+                username: row.get(3),
+                email: row.get(4),
+                password: row.get(5),
+            };
+            
+            (OK_RESPONSE.to_string(), serde_json::to_string(&user).unwrap())
         }
-        _ => return (NOT_FOUND.to_string(), "Internal error".to_string()),
+        _ => (NOT_FOUND.to_string(), "User not found".to_string()),
     }
 }
 
-fn is_email_username_available(email: &str, username: &str, client: &mut Client) -> bool {
-    // Check if email or username already exists
-    match client.query_opt("SELECT 1 FROM users WHERE email = $1 OR username = $2 LIMIT 1", &[&email, &username]) {
-        Ok(Some(_)) => false, // Email or username already exists
-        _ => true, // Email and username are available
+
+
+fn handle_update_request(request: &str) -> (String, String) {
+    //request la irkra user id ah parse function return panum 
+    let id:i32;
+    if let Some(id1) = parse_user_id(request) {
+        id = id1;
+    } else {
+        return (NOT_FOUND.to_string(), "Invalid request body".to_string());
+    }
+    println!("put user id :{}", id);
+    match get_user_request_body(request) {
+        Ok(user) => {
+            match Client::connect(DB_URL, NoTls) {
+                Ok(mut client) => {
+                    
+                    if is_email_exists(&user.email, &mut client) {return (NOT_FOUND.to_string(),"User with the given email already exists".to_string());}
+                    if is_username_exists(&user.username, &mut client) {return (NOT_FOUND.to_string(),"User with the given username already exists".to_string());}
+                    
+                    // Update the user details in the database
+                    match client.execute(
+                        "UPDATE users SET firstname = $1, lastname = $2, username = $3, email = $4, password = $5 WHERE id = $6",
+                        &[&user.firstname, &user.lastname, &user.username, &user.email, &user.password, &id],
+                    ) {
+                        Ok(_) => {
+                            // Fetch and return the updated user details
+                            match client.query_one(
+                                "SELECT id, firstname, lastname, username, email, password FROM users WHERE id = $1",
+                                &[&id],
+                            ) {
+                                Ok(row) => {
+                                    let updated_user = User {
+                                        id: row.get(0),
+                                        firstname: row.get(1),
+                                        lastname: row.get(2),
+                                        username: row.get(3),
+                                        email: row.get(4),
+                                        password: row.get(5),
+                                    };
+                                    return (OK_RESPONSE.to_string(), serde_json::to_string(&updated_user).unwrap());
+                                }
+                                Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to retrieve updated user".to_string()),
+                            }
+                        }
+                        Err(_) => return (INTERNAL_ERROR.to_string(), "Failed to update user".to_string()),
+                    }
+                }
+                Err(_) => return (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+            }
+        }
+        _ => return (NOT_FOUND.to_string(), "Invalid request body".to_string()),
     }
 }
 
@@ -194,7 +288,7 @@ fn handle_get_all_request(_request: &str) -> (String, String) {
     match Client::connect(DB_URL, NoTls) {
         Ok(mut client) => {
             let mut users = Vec::new();
-
+            
             for row in client.query("SELECT id, firstname, lastname, username, email, password FROM users", &[]).unwrap() {
                 users.push(User {
                     id: row.get(0),
@@ -205,12 +299,13 @@ fn handle_get_all_request(_request: &str) -> (String, String) {
                     password: row.get(5),
                 });
             }
-
+            
             (OK_RESPONSE.to_string(), serde_json::to_string(&users).unwrap())
         }
         _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
     }
 }
+
 
 
 fn handle_login_request(request: &str) -> (String, String) {
@@ -246,7 +341,7 @@ fn handle_login_request(request: &str) -> (String, String) {
                                         email: row.get(4),
                                         password: row.get(5),
                                     };
-
+                                    
                                     (OK_RESPONSE.to_string(), serde_json::to_string(&authenticated_user).unwrap())
                                 }
                                 _ => (NOT_FOUND.to_string(), "Invalid email or password".to_string()),
@@ -267,21 +362,59 @@ fn handle_login_request(request: &str) -> (String, String) {
 
 
 
-
+//######################################################################################################//
+// -------------------------------------------- helper functions----------------------------------------// 
+//######################################################################################################//
 
 
 fn get_user_request_body(request: &str) -> Result<User, serde_json::Error> {
-
     println!("request in get user body \n{}\n\n",request);
     let json_str = request.split("\r\n\r\n").last().unwrap_or_default();
-    println!("request in get user body (json){}\n\n",json_str);
-    
+    println!("request in get user body (json){}\n\n",json_str);   
     // Deserialize the JSON string into a User struct
     serde_json::from_str(json_str).map_err(|err| {
-        // Print debug information
         eprintln!("Error parsing JSON(in reqBody):  {:?}", err);
-        // Return the original error
         err
     })
 }
 
+fn parse_user_id(request: &str) -> Option<i32> {
+    // Extract user ID from the request path
+    
+    let path_parts: Vec<&str> = request.split('/').collect();
+    println!("{:?}",path_parts);
+    if path_parts.len() >= 5 && path_parts[3] == "users" {
+        println!("######path paths 4: {}",path_parts[4]);
+        if let Ok(id) = path_parts[4].split_whitespace().next().unwrap().parse::<i32>() {
+            return Some(id);
+        }
+    }
+    None
+}
+
+
+fn is_email_exists(email: &str, client: &mut Client) -> bool{
+    match client.query_opt("SELECT 1 FROM users WHERE email = $1", &[&email]){
+        Ok(Some(_))=>true, //databasela intha mail irku
+        _=>false, //illa
+    }
+}
+
+fn is_username_exists(email: &str, client: &mut Client) -> bool{
+    match client.query_opt("SELECT 1 FROM users WHERE username = $1", &[&email]){
+    Ok(Some(_))=>true, //databasela intha username irku
+        _=>false,//illa
+    }
+}
+
+// fn is_email_username_available(email: &str, username: &str, client: &mut Client) -> bool {
+//     // Check if email or username already exists
+//     match client.query_opt("SELECT 1 FROM users WHERE email = $1 OR username = $2 LIMIT 1", &[&email, &username]) {
+//         Ok(Some(_)) => false, // Email or username already exists
+//         _ => true, // Email and username are available
+//     }
+// }
+
+
+// update pnaumbothu same username email irka nu check pnanum
+// simple checking la thaniya section create pni implement pnanum...eg: get_user_request_body, is_email_exists  
